6d27db5fbbf4eaf6ce6bdfb42a009c28d4a94a9e
sharpcdf <csharpdfjer@gmail.com>
2022-07-17 14:30:22 -0400

    remove help.nim

--- jitter.nim
+++ jitter.nim
@@ -1,5 +1,4 @@
 import std/[terminal, os]
-include src/help
 
 var args: seq[string]
 const version {.strdefine.} = "undefined"
@@ -10,6 +9,23 @@ when not declared(commandLineParams):
 else:
     args = commandLineParams()
 
+proc printHelp() =
+    echo """Usage:
+        jtr <command> [args]
+
+    install [gh:|gl:|cb:|sh:]<[user/]repo>             Installs the binaries of the given repository, if avaliable.
+    update <[user/]repo>                               Updates the specified binaries, or all binaries if none are specified.
+    remove <[user/]repo>                               Removes the specified binaries from your system.
+    search [gh:|gl:|cb:|sh:]<[user/]repo>              Searches for binaries that match the given repositories, returning them if found.
+    list [gh|gl|cb|sh]                                 Lists binaries downloaded from the specified source, or all if nothing is specified.
+    help                                               Prints this help.
+    version                                            Prints the version of jitter
+
+    --replace                                          Removes all other binaries with the same name after updating/installing
+    --version=<tag>                                    Specifies a version to download                                          
+    -ver=<tag>
+    """
+    
 if (args.len == 1 and args[0] == "help") or args.len == 0:
     printHelp()
     quit()
